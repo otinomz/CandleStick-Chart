f5660e0690fa0a03e42d4b0dccea18a860c7534c
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-06 08:39:35 +0100

    creating chart JS component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import ChartJS from "./components/Chart";
 
 const App = () => {
   return (
     <div className="App">
       <h1>CandleStick Stock Chart</h1>
+      <ChartJS />
     </div>
   );
 };
---
+++ src/components/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartJS = () => {
+  return <div>Chart Js section</div>;
+};
+
+export default ChartJS;
