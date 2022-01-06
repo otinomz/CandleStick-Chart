2b8822ec515fcf51f936547a4221a92a25e9f56c
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-06 09:16:38 +0100

    setting up the useState hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState } from "react";
 import "./styles.css";
 import ChartJS from "./components/Chart";
+import MadeData from "./components/Data";
 
 const App = () => {
+  const [chartsToDisplay, setChartsToDisplay] = useState([]);
   return (
     <div className="App">
       <h1>CandleStick Stock Chart</h1>
