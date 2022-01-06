5bc979bf7e3c5aee184eeae68b524d6747cedb61
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-06 08:53:40 +0100

    adding properties for the chart Canvas

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { scaleTime } from "d3-scale";
-import madeData from "./Data";
+import MadeData from "./Data";
 import { ChartCanvas, Chart } from "react-stockcharts";
 import { CandleStickSeries } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
@@ -9,8 +9,23 @@ import { utcDay } from "d3-time-format";
 import { fitWidth } from "react-stockcharts/lib/helper";
 import { timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 
-const ChartJS = () => {
-  return <div>Chart Js section</div>;
+let ChartJS = (props) => {
+  const { type, width, ratio } = props;
+  const data = MadeData;
+  const xAccessor = (d) => {
+    return d.date;
+  };
+  return (
+    <div className="ChartJS">
+      <ChartCanvas>
+        height={400}
+        ratio={ratio}
+        width={width}
+        {/* as stated in the documentaion */}
+        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+      </ChartCanvas>
+    </div>
+  );
 };
 
 export default ChartJS;
