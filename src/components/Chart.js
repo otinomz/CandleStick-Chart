78152f2329e69b91bd3e83c2abf95fa80d4b5fe8
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-06 08:47:23 +0100

    importing dependencies into chartjs componennt

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { scaleTime } from "d3-scale";
+import madeData from "./Data";
+import { ChartCanvas, Chart } from "react-stockcharts";
+import { CandleStickSeries } from "react-stockcharts/lib/series";
+import { XAxis, YAxis } from "react-stockcharts/lib/axes";
+import { utcDay } from "d3-time-format";
+import { fitWidth } from "react-stockcharts/lib/helper";
+import { timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 
 const ChartJS = () => {
   return <div>Chart Js section</div>;
